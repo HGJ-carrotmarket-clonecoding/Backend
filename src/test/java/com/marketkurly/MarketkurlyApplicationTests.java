be02c178bf6a667499fa1545ae2aefbf70bd7386
habel195 <worbs9404@hanamil.net>
2022-04-18 15:52:38 +0900

    setting

---
+++ src/test/java/com/marketkurly/MarketkurlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketkurly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketkurlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
