3f3d010159b6fdba3bd5e5352b62417e8555e486
MINYA <minya8703@gmail.com>
2022-04-25 13:48:11 +0900

    API 설명및 코ì드 코멘트 추ã가

--- src/main/java/com/marketkurly/service/ProductService.java
+++ src/main/java/com/marketkurly/service/ProductService.java
@@ -20,11 +20,13 @@ import java.util.List;
 @RequiredArgsConstructor
 @Service
 public class ProductService {
+    // 네이버 검색의 쇼핑 검색 결과를 반환 API 예제
 
     private final ProductRepository productRepository;
 
-    private final String CLIENT_ID = "fm4NYvCHEDw8lwU9K78x";
-    private final String CLIENT_SECRET = "5oMRZB9nq8";
+    // 네이버 검색의 쇼핑 검색 결과를 반환
+    private final String CLIENT_ID = "fm4NYvCHEDw8lwU9K78x"; //애플리케이션 클라이언트 아이디값";
+    private final String CLIENT_SECRET = "5oMRZB9nq8"; //애플리케이션 클라이언트 시크릿값";
     private final String OpenApiNaverShopUrl = "https://openapi.naver.com/v1/search/shop.json?display=50&query=";
     int display = 15; //1차 프레임에서는 20으로 고정
     int start = 0; // 1차 프레임에서는 0으로 고정
@@ -33,10 +35,13 @@ public class ProductService {
         page -= 1;
         Page<Product> products = productRepository.findByNameLike("%" + keyword + "%", PageRequest.of(page, display));
         if (products.isEmpty()) {
+            // 네이버 쇼핑 검색 API호출
             String apiBody = getProductsFromApi(keyword, page);
+            // API로 얻은 데이터를 JSON방식으로 변경
             List<ProductRequestDto> productApi = fromJSONtoItems(apiBody);
 //            products = new PageImpl<>(productApi, PageRequest.of(page, display), display);
 
+            //
             List<Product> productList = new ArrayList<>();
             for (ProductRequestDto productDto : productApi) {
                 productList.add(new Product(productDto));
@@ -58,7 +63,9 @@ public class ProductService {
 
         start = page == 0 ? 1 : page * display + 1;
         HttpEntity<String> requestEntity = new HttpEntity<>(body, headers);
+        // API 호출
         ResponseEntity<String> responseEntity = restTemplate.exchange(OpenApiNaverShopUrl + keyword + "&start=" + start, HttpMethod.GET, requestEntity, String.class);
+        // API 호출 결과 상태값
         HttpStatus httpStatus = responseEntity.getStatusCode();
         System.out.println("Response Status : " + httpStatus.value());
         System.out.println(responseEntity.getBody());
@@ -67,6 +74,7 @@ public class ProductService {
 
 
     public List<ProductRequestDto> fromJSONtoItems(String result) {
+        // JSON 라이브러리를 사용하여 파싱
         JSONObject json = new JSONObject(result);
         JSONArray items = json.getJSONArray("items");
         List<ProductRequestDto> productDtos = new ArrayList<>();
