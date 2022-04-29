a55bbb4bc5906357a87a4d3e40a94e447f28bc75
minya8703 <minya8703@gmail.com>
2022-04-29 14:58:14 +0900

    exception 처리 수정

---
+++ src/main/java/com/marketkurly/exception/RestApiExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.marketkurly.exception;
+
+
+import com.marketkurly.dto.responseDto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RestApiExceptionHandler {
+    @ExceptionHandler(value = {
+            DuplicateUserException.class,
+            EmailFormException.class,
+            EmptyException.class,
+            JwtTokenExpiredException.class,
+            LengthException.class,
+            NoItemException.class,
+            TokenNullException.class,
+            UnauthenticatedException.class,
+            NoneLoginException.class,
+            UsernameNotFoundException.class
+    })
+    public ResponseEntity<Object> RestApiExceptionHandle(RuntimeException ex) {
+        ResponseDto restApiException = new ResponseDto("failed", ex.getMessage(), "");
+        return new ResponseEntity<>(
+                restApiException,
+                HttpStatus.BAD_REQUEST
+        );
+    }
+}
